99d25e81a0d52fe4bdf3c926946778986aeabe17
Sonia364 <c0872364@mylambton.ca>
2022-12-07 23:34:01 +0000

    Initial Commit

---
+++ app/src/main/java/com/sonia/the_achievers_final_project_mad3125/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sonia.the_achievers_final_project_mad3125;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
