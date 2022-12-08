34d96183f9309025036839b8fc6ccb1514b73f5c
Rabia790 <rabiaangel9921@gmail.com>
2022-12-08 18:21:28 -0500

    Added Validations for email and password

--- app/src/main/java/com/sonia/the_achievers_final_project_mad3125/MainActivity.java
+++ app/src/main/java/com/sonia/the_achievers_final_project_mad3125/MainActivity.java
@@ -3,12 +3,39 @@ package com.sonia.the_achievers_final_project_mad3125;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText email;
+    EditText password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.btnLogin);
+
+        loginButton.setOnClickListener(
+                v-> {
+                if (email.getText().toString().equals("")) {
+                    Toast.makeText(this, "Please enter your Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.getText().toString().equals("")) {
+                    Toast.makeText(this, "Please enter your Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+        });
+
     }
+
+
 }
