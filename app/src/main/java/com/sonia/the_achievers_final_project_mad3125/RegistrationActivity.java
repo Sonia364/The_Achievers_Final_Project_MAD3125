3634e028cd7c0c8571b549d9f134df4f95d9e1a0
kanduriabhilash <kanduriabhilash2@gmail.com>
2022-12-08 20:12:53 -0500

    Added spinner in the UI of Registration Activity.

--- app/src/main/java/com/sonia/the_achievers_final_project_mad3125/RegistrationActivity.java
+++ app/src/main/java/com/sonia/the_achievers_final_project_mad3125/RegistrationActivity.java
@@ -3,12 +3,21 @@ package com.sonia.the_achievers_final_project_mad3125;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        spinner = findViewById(R.id.emp_type);
+
+        Spinner spinner = (Spinner) findViewById(R.id.emp_type);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
