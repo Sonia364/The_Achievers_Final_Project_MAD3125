c90ead1166a6fbba89dfa992ded9aaa8d2028929
kanduriabhilash <kanduriabhilash2@gmail.com>
2022-12-08 21:38:49 -0500

    Added Intent in MainActivity,EmployeedashboardActivity,RegistrationActivity

--- app/src/main/java/com/sonia/the_achievers_final_project_mad3125/EmployeeDashboard.java
+++ app/src/main/java/com/sonia/the_achievers_final_project_mad3125/EmployeeDashboard.java
@@ -3,12 +3,25 @@ package com.sonia.the_achievers_final_project_mad3125;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
 
 public class EmployeeDashboard extends AppCompatActivity {
+
+    Button addRecord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_dashboard);
+
+        addRecord = findViewById(R.id.add_new);
+
+        addRecord.setOnClickListener(
+                v-> {
+                    Intent intent = new Intent(EmployeeDashboard.this, RegistrationActivity.class);
+                    startActivity(intent);
+                });
+
     }
 }
