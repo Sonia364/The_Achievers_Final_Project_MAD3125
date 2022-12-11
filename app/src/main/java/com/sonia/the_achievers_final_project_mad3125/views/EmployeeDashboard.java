9b394385397c8fe7e99f0e15837a2695eb3b23e1
Rabia790 <rabiaangel9921@gmail.com>
2022-12-11 15:48:26 -0500

    Added models package and implemented floating button in employee dashboard activity.

--- app/src/main/java/com/sonia/the_achievers_final_project_mad3125/views/EmployeeDashboard.java
+++ app/src/main/java/com/sonia/the_achievers_final_project_mad3125/views/EmployeeDashboard.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 import com.sonia.the_achievers_final_project_mad3125.R;
 
 public class EmployeeDashboard extends AppCompatActivity {
 
-    Button addRecord;
+    FloatingActionButton addRecord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
